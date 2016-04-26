76a6c3e43fbb4516412dd0fb68a7704f1d6676e5
destpat <yoan.ficadiere@gmail.com>
2016-04-26 10:40:42 +0200

    reparer

---
+++ app/routes/recette.js
@@ -0,0 +1,9 @@
+// ROUTES RECETTE
+var Recette = require('../models/recette.js');
+module.exports = function(app) {
+
+    app.get('/recettes', Recette.findAll);
+    app.post('/recettes', Recette.create);
+    app.put('/recettes/:id', Recette.update);
+    app.delete('/recettes/:id', Recette.delete);
+}
