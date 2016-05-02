d497a438ef0763b2e0882170dc82b711c0eaec74
Jerem <j.bouche33@gmail.com>
2016-05-02 17:04:44 +0200

    update style.css mainController main.html

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,7 +1,12 @@
-function mainController($scope, $rootScope, $http) {
+function mainController($scope, $rootScope, $http,recetteService) {
 	$('body').css('background-image', 'none');
 	$scope.showRecette = 'entree';
-
+	function load() {
+		recetteService.get().then(function (res) {
+			$scope.recettes = res.data;
+		});
+	}
+	load();
 
 	$scope.menuShow = function (n) {
 		angular.element($('#'+$scope.showRecette)).removeClass( "boutonActive" );
