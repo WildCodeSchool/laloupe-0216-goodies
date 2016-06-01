8024702fe24e515da4ed00f50a8365e322a2a7e8
Ludo-Boa <ludo.bolla@gmail.com>
2016-06-01 14:05:16 +0200

    update style.css + index.html + maincontroller.js + createEvent.html + events.html + main.html

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,6 +1,14 @@
 function mainController($scope, $rootScope, $http,recetteService) {
 	$('body').css('background-image', 'none').css('background-image','url("./assets/backhome.jpg")');
 	$scope.showRecette = 'entree';
+
+	$('.slide').on(function(){
+		$('.flex-caption1').fadeToggle("1000");
+		$('.flex-caption2').fadeToggle("1000");
+		$('.flex-caption3').fadeToggle("1000");
+		
+	});
+
 	function load() {
 		recetteService.get().then(function (res) {
 			$scope.recettes = res.data;
