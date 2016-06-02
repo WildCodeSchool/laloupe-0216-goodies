028f86f93c9f3ec48034b69dd5740c8623857b0b
Jerem <j.bouche33@gmail.com>
2016-06-02 09:40:58 +0200

    fux bug add firend create eventController

--- public/js/controllers/eventController.js
+++ public/js/controllers/eventController.js
@@ -1,13 +1,9 @@
 
 // eventController ==============================
 
-function eventController($scope, $http, eventService, friendService, $location, recetteService, $rootScope, userService, NgMap) {
+function eventController($scope, $http, eventService, friendService, $location, recetteService, $rootScope, userService) {
 	load();
-	$scope.position = 'current-location';
-  NgMap.getMap().then(function(map) {
-    $scope.map = map;
-  });
-	
+
 	$scope.geocodeAddress = function (address) {
 		$scope.position = [address.position[0],address.position[1]];
   }
