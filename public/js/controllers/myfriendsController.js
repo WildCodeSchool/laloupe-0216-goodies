324eddf7b71caa96748561a80ee30134ec49a0ad
Jerem <j.bouche33@gmail.com>
2016-06-03 14:08:01 +0200

    fix bug myfriendController

--- public/js/controllers/myfriendsController.js
+++ public/js/controllers/myfriendsController.js
@@ -8,6 +8,8 @@ function myfriendsController(userService, friendService, $scope, $rootScope) {
       });
       friendService.get().then(function(res){
         $scope.friends = res.data;
+        console.log('fffff');
+        console.log($scope.friends);
       });
     }
     load();
@@ -17,9 +19,11 @@ function myfriendsController(userService, friendService, $scope, $rootScope) {
 
       userService.findByNameSurname(user[1],user[0]).then(function(res){ // ===== Récupération de l'ID du Friend
     	    datas.friendId = res.data.id;
+          console.log(datas.friendId);
+          console.log(res.data.id);
           datas.img = res.data.img;
 
-          if ($rootScope.userId != $scope.userFriendId && $scope.friends.map(function (e)
+          if ($rootScope.userId != datas.friendId && $scope.friends.map(function (e)
           {if(e.nom == user[1] && e.prenom == user[0] && e.userId == $rootScope.userId)
             return true;}).indexOf(true) == -1){
 
