9680ffca14603d2cfd3b64bd8b0c5ca2a81794ba
Ludo-Boa <ludo.bolla@gmail.com>
2016-06-03 14:04:45 +0200

    creation container down - main.html

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -2,6 +2,22 @@ function mainController($scope, $rootScope, $http,recetteService) {
 	$('body').css('background-image', 'none').css('background-image','url("./assets/backhome.jpg")');
 	$scope.showRecette = 'entree';
 
+	$(function(){
+  var front = $('.Front'),
+      others = ['Left2', 'Left', 'Right', 'Right2'];
+  
+  $('.Carouseltest').on('click', '.Items', function() {
+    var $this = $(this);
+    
+    $.each(others, function(i, cl) {
+      if ($this.hasClass(cl)) {
+        front.removeClass('Front').addClass(cl);
+        front = $this;
+        front.addClass('Front').removeClass(cl);
+      }
+    });
+  });
+});
 
 	function load() {
 		recetteService.get().then(function (res) {
