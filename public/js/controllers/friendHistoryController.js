ed8d6b53464b310250f83b081be9afd20cb8535d
Jerem <j.bouche33@gmail.com>
2016-06-28 17:46:56 +0200

    fix notification add repeat list invit friends events

--- public/js/controllers/friendHistoryController.js
+++ public/js/controllers/friendHistoryController.js
@@ -1,20 +1,35 @@
 
-function friendHistoryController($scope, $routeParams, userFactory) {
+function friendHistoryController($scope, $routeParams, userFactory, $location) {
     function load() {
         $scope.events = userFactory.user.events;
-        console.log('reerererer');
-        console.log($scope.events);
         $scope.history = [];
+        for (var j = 0; j < userFactory.user.friends.length; j++) {
+          if (userFactory.user.friends[j]._id == $routeParams.id) {
+              $scope.user = userFactory.user.friends[j];
+          }
+        }
         for (var i = 0; i < $scope.events.length; i++) {
             for (var j = 0; j < $scope.events[i].tabFriendEvent.length; j++) {
-              if ($scope.events[i].tabFriendEvent == $routeParams.id) {
+              if ($scope.events[i].tabFriendEvent[j] == $routeParams.id) {
                   $scope.history.push($scope.events[i]);
               }
             }
         }
-        console.log('hist');
-        console.log($scope.history);
     }
     load();
-
+    $scope.info = function(id,option) {
+      console.log(id);
+      var name = '';
+      var prenom = '';
+      for (var j = 0; j < userFactory.user.friends.length; j++) {
+        if (userFactory.user.friends[j]._id == id) {
+            name = userFactory.user.friends[j].name;
+            prenom = userFactory.user.friends[j].prenom;
+        }
+      }
+      return option == 'nom' ? name : prenom;
+    }
+    $scope.friendhistory = function (id){
+      $location.path('/friendhistory/'+id);
+    }
 }
